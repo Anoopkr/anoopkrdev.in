df3a4c9609cd7a9ff88f1928077ced63c2fbffad
Anoop Kumar <anoopply2009@gmail.com>
2018-07-28 20:04:33 +0530

    Added contact me functionality

---
+++ js/controller.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp',[]);
+
+app.controller('HomeController', function($scope, $http) {
+
+$scope.data = {};
+$scope.errorMessage = "";
+$scope.message = "";
+	$scope.sendMail = function(){
+		console.log("tttt");
+		 $http({
+        method : "POST",
+        url : "https://8l47mk9ct3.execute-api.us-east-1.amazonaws.com/prod/mail",
+        data: $scope.data
+    }).then(function mySuccess(response) {
+    	$scope.data = {};
+    	$scope.message = "Message sent. Thank you!"
+    }, function myError(response) {
+    	$scope.errorMessage = "Message sending failed. Please send the message through my mail id."
+    });
+	}
+
+});
